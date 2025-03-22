f170fdc719663ec0af64b14b96081960036e1ce6
macbook <83575828+m-mittendorfer@users.noreply.github.com>
2025-03-22 11:11:22 +0100

    added

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,181 @@
+var store = [{
+        "title": "Portfolio item number 1",
+        "excerpt":"This is an item in your portfolio. It can be have images or nice text. If you name the file .md, it will be parsed as markdown. If you name the file .html, it will be parsed as HTML.  ","categories": [],
+        "tags": [],
+        "url": "/portfolio/portfolio-1/",
+        "teaser": null
+      },{
+        "title": "Portfolio item number 2",
+        "excerpt":"This is an item in your portfolio. It can be have images or nice text. If you name the file .md, it will be parsed as markdown. If you name the file .html, it will be parsed as HTML.  ","categories": [],
+        "tags": [],
+        "url": "/portfolio/portfolio-2/",
+        "teaser": null
+      },{
+        "title": "Blog Post number 1",
+        "excerpt":"This is a sample blog post. Lorem ipsum I can’t remember the rest of lorem ipsum and don’t have an internet connection right now. Testing testing testing this blog post. Blog posts are cool.   Headings are cool   You can have many headings   Aren’t headings cool?  ","categories": [],
+        "tags": ["cool posts","category1","category2"],
+        "url": "/posts/2012/08/blog-post-1/",
+        "teaser": null
+      },{
+        "title": "Blog Post number 2",
+        "excerpt":"This is a sample blog post. Lorem ipsum I can’t remember the rest of lorem ipsum and don’t have an internet connection right now. Testing testing testing this blog post. Blog posts are cool.   Headings are cool   You can have many headings   Aren’t headings cool?  ","categories": [],
+        "tags": ["cool posts","category1","category2"],
+        "url": "/posts/2013/08/blog-post-2/",
+        "teaser": null
+      },{
+        "title": "Blog Post number 3",
+        "excerpt":"This is a sample blog post. Lorem ipsum I can’t remember the rest of lorem ipsum and don’t have an internet connection right now. Testing testing testing this blog post. Blog posts are cool.   Headings are cool   You can have many headings   Aren’t headings cool?  ","categories": [],
+        "tags": ["cool posts","category1","category2"],
+        "url": "/posts/2014/08/blog-post-3/",
+        "teaser": null
+      },{
+        "title": "Blog Post number 4",
+        "excerpt":"This is a sample blog post. Lorem ipsum I can’t remember the rest of lorem ipsum and don’t have an internet connection right now. Testing testing testing this blog post. Blog posts are cool.   Headings are cool   You can have many headings   Aren’t headings cool?  ","categories": [],
+        "tags": ["cool posts","category1","category2"],
+        "url": "/posts/2012/08/blog-post-4/",
+        "teaser": null
+      },{
+        "title": "Future Blog Post",
+        "excerpt":"This post will show up by default. To disable scheduling of future posts, edit config.yml and set future: false.  ","categories": [],
+        "tags": ["cool posts","category1","category2"],
+        "url": "/posts/2012/08/blog-post-4/",
+        "teaser": null
+      },{
+        "title": "A case for phonology in Cognitive Grammar: vowel reduction in Standard Southern British",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/publication/2021-Phonology-CG",
+        "teaser": null
+      },{
+        "title": "Discourse functions, placement and prosody: An FDG analysis of left- and right-dislocation in British English",
+        "excerpt":"This paper presents an analysis of the discourse features, linearisation and prosody of left and right dislocated elements in spoken British English. Using data from the British component of the International Corpus of English, the relevance of the notion of Topic in the analysis of dislocation is rejected. The paper...","categories": [],
+        "tags": [],
+        "url": "/publication/2025-Dislocation",
+        "teaser": null
+      },{
+        "title": "Pushing the boundaries of Cognitive Grammar: a case for phonology within a cognitive linguistic framework",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/talks/2019-Phonology-CG",
+        "teaser": null
+      },{
+        "title": "Towards an operationalisation of Contrast",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/talks/2022-OperationalisationContrast",
+        "teaser": null
+      },{
+        "title": "Identifying Contrast in corpus data: challenges and possible solutions",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/talks/2022-IdentifyingContrast",
+        "teaser": null
+      },{
+        "title": "Functional Discourse Grammar: an introduction (guest lecture)",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/talks/2023-IntroductionFDG",
+        "teaser": null
+      },{
+        "title": "Discourse functions and placement: an analysis of dislocation in English",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/talks/2023-ISLE7",
+        "teaser": null
+      },{
+        "title": "Discourse functions, placement and prosody: an FDG analysis of left- and right-dislocation in British English",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/talks/2023-IWFDG",
+        "teaser": null
+      },{
+        "title": "Left- and right-dislocation in discourse: discourse functions, word order and prosody",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/talks/2023-SLE56",
+        "teaser": null
+      },{
+        "title": "Exploring the prosody of information packaging in British English: an FDG account",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/talks/2024-ICFDG",
+        "teaser": null
+      },{
+        "title": "Mismatches between information structure and prosody: Contrast-marking in spoken British English",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/talks/2024-SLE57",
+        "teaser": null
+      },{
+        "title": "The pragmatics, word order and prosody of preposing constructions: evidence from spoken British English.",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/talks/2024-Rountable",
+        "teaser": null
+      },{
+        "title": "The prosodic realization of information packaging: evidence from British English",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/talks/2024-OLS",
+        "teaser": null
+      },{
+        "title": "TR BEd 02.1: Grammar in Use (WS 2018)",
+        "excerpt":"Taught a weekly tutorial on English Syntax &amp; Grammar. Topics covered included, among others, constituency structure, NP structure, tense &amp; aspect, information packaging constructions.   ","categories": [],
+        "tags": [],
+        "url": "/teaching/2018-winter-teaching",
+        "teaser": null
+      },{
+        "title": "TR BEd 02.1: Grammar in Use (SS 2019)",
+        "excerpt":"Taught a weekly tutorial on English Syntax &amp; Grammar. Topics covered included, among others, constituency structure, NP structure, tense &amp; aspect, information packaging constructions.   ","categories": [],
+        "tags": [],
+        "url": "/teaching/2019-spring-teaching",
+        "teaser": null
+      },{
+        "title": "TR BEd 02.1: Grammar in Use (WS 2019)",
+        "excerpt":"Taught a weekly tutorial on English Syntax &amp; Grammar. Topics covered included, among others, constituency structure, NP structure, tense &amp; aspect, information packaging constructions.   ","categories": [],
+        "tags": [],
+        "url": "/teaching/2019-winter-teaching",
+        "teaser": null
+      },{
+        "title": "TR BEd 02.1: Grammar in Use (SS 2020)",
+        "excerpt":"Taught a weekly tutorial on English Syntax &amp; Grammar. Topics covered included, among others, constituency structure, NP structure, tense &amp; aspect, information packaging constructions.   ","categories": [],
+        "tags": [],
+        "url": "/teaching/2020-spring-teaching",
+        "teaser": null
+      },{
+        "title": "PS BA Proseminar Linguistics 1 (WS 2022)",
+        "excerpt":"Taught a weekly proseminar on academic writing skills with a thematic focus on the English noun phrase. Topics covered included, among others, NP structure, headedness, pre- and post-head dependents, as well as research skills (e.g. plagiarism, correct referencing, paper structure).  ","categories": [],
+        "tags": [],
+        "url": "/teaching/2022-winter-teaching",
+        "teaser": null
+      },{
+        "title": "PS BA Proseminar Linguistics 1 (SS 2023)",
+        "excerpt":"Taught a weekly proseminar on academic writing skills with a thematic focus on the English noun phrase. Topics covered included, among others, NP structure, headedness, pre- and post-head dependents, as well as research skills (e.g. plagiarism, correct referencing, paper structure).  ","categories": [],
+        "tags": [],
+        "url": "/teaching/2023-summer-teaching",
+        "teaser": null
+      },{
+        "title": "VO Introduction to the Study of Language 2 (WS 2023; co-taught with Evelien Keizer)",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/teaching/2023-winter-teaching1",
+        "teaser": null
+      },{
+        "title": "AR Phonetic Transcription (WS 2023)",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/teaching/2023-winter-teaching2",
+        "teaser": null
+      },{
+        "title": "VO Introduction to the Study of Language 2 (WS 2024; co-taught with Evelien Keizer)",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/teaching/2023-winter-teaching1",
+        "teaser": null
+      },{
+        "title": "AR Phonetic Transcription (WS 2024)",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/teaching/2024-winter-teaching1",
+        "teaser": null
+      }]
